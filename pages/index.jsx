452fadb4fe3bbbeaed6c8d67ab109265bff4b3c0
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-16 06:18:02 +0700

    Modified: Update index.js for using all  new components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,22 @@
-import Image from "next/image";
-import { Poppins } from "next/font/google";
-import { useDispatch } from "react-redux";
-// import { fetchDataMovie } from "@/redux/slices/slice-search";
 import { SearchHome } from "@/components/Molecules/search-Home";
+import { TabsSortBy } from "@/components/Molecules/tabsSortBy";
+import baseAPI from "@/services/API/Base-API";
 import axios from "axios";
-import Data from "@/utils/API/Base-API";
-import dataHomePage from "@/utils/Data/homepage";
-import CardMovie from "@/components/Templates/CardMovie/CardMovie";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   weight: "400",
   subsets: ["latin"],
 });
 
-export default function Home({ dataMovies }) {
-  const dispatch = useDispatch();
-
-  const handleGetData = () => {
-    // dispatch(fetchDataMovie("t=spiderman&plot=full"));
-  };
-
+export default function Home({ dataTrending, dataPopular, dataUpcoming }) {
   return (
     <section
       className={`flex min-h-screen flex-col items-center justify-between ${poppins.className}`}
     >
-      <div className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="container grid items-center pb-8 pt-6 md:py-10">
         <div className="flex flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-4">
             Welcome.
             <br className="hidden sm:inline" />
             Millions of movies, TV shows and people to discover. <br /> Explore
@@ -35,49 +25,45 @@ export default function Home({ dataMovies }) {
           <SearchHome />
         </div>
 
-        <div className="flex flex-row justify-between">
-          <div className="font-semibold text-[22px] text-black mb-4">
-            Movies
-          </div>
-          <div className="font-semibold text-lg text-black mb-4">See all</div>
-        </div>
-        <div className="grid grid-rows-1 grid-cols-4 gap-5">
-          {dataMovies.map((film) => (
-            <CardMovie
-              urlImage={film?.Poster}
-              title={film?.Title}
-              year={film?.Year}
-              rating={film?.imdbRating}
-              key={film?.imdbID}
-            />
-          ))}
-        </div>
+        <TabsSortBy
+          dataTrending={dataTrending}
+          dataPopular={dataPopular}
+          dataUpcoming={dataUpcoming}
+        />
       </div>
     </section>
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   try {
-    const dataMovies = await Promise.all(
-      dataHomePage.map(async (movie) => {
-        const res = await axios.get(Data(`t=${movie}&plot=full`));
-        return res.data;
-      })
+    const responseTrending = await axios.request(
+      baseAPI(`movie/now_playing?language=en-US&page=1`)
     );
-
-    // console.log(dataMovies);
+    const responsePopular = await axios.request(
+      baseAPI(`movie/popular?language=en-US&page=1`)
+    );
+    const responseUpcoming = await axios.request(
+      baseAPI(`movie/upcoming?language=en-US&page=1`)
+    );
+    const dataTrending = responseTrending.data.results;
+    const dataPopular = responsePopular.data.results;
+    const dataUpcoming = responseUpcoming.data.results;
 
     return {
       props: {
-        dataMovies,
+        dataTrending,
+        dataPopular,
+        dataUpcoming,
       },
     };
   } catch (error) {
-    console.log("errorFetchingAPI", error);
+    console.log("errorFetchingHomeAPI", error);
     return {
       props: {
-        dataMovies: [],
+        dataTrending: [],
+        dataPopular: [],
+        dataUpcoming: [],
       },
     };
   }
