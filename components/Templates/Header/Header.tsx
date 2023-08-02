e498ded54bae2271c08bee73412fa3c17e625fe5
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-03 00:46:17 +0700

    feat(Navbar): Update padding and margin style for responsiveness

--- components/Templates/Header/Header.tsx
+++ components/Templates/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "../Navbar/Navbar";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-center sticky top-0 z-30 md:h-[60px] h-[110px] w-full shadow-md backdrop-blur-xl">
+    <header className="flex items-center justify-center sticky top-0 z-30 pb-2 md:pb-0 md:h-[60px] w-full shadow-md backdrop-blur-xl">
       <Navbar />
     </header>
   );
