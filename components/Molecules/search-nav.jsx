dd54712cc0660ddca7d4dfbcad24c77bb5b53527
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-03 00:30:28 +0700

    feat(search): Update style max-width for responsiveness

--- components/Molecules/search-nav.jsx
+++ components/Molecules/search-nav.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function NavSearch({ value, onChange, onKeyPress }) {
   return (
-    <div className="border-2 sm:py-1 sm:px-2 rounded-full bg-white text-sm">
+    <div className="border-2 sm:py-1 px-2 rounded-full bg-white text-sm">
       <i className="fas fa-search relative" aria-hidden="true"></i>
       <input
-        className="py-1 px-2 focus:outline-none relative w-40 sm:full"
+        className="py-1 px-2 focus:outline-none relative w-[90px] sm:w-40"
         type="text"
         placeholder="Search"
         value={value}
