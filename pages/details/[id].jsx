92a4a1c18e22a936025dcde8e1517d9a39e5f7f6
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-03 01:32:04 +0700

    feat(Dynamic Route Page): Update Positions, witdh,padding, and margin style for responsiveness

--- pages/details/[id].jsx
+++ pages/details/[id].jsx
@@ -21,13 +21,13 @@ export default function Detail() {
   const year = currentDate.getFullYear();
 
   return (
-    <div className={`mt-8 mb-60 ${fontSans.className}`}>
+    <div className={`mt-8 mb-5 ${fontSans.className}`}>
       {loading === true ? (
         <SkeletonDetail />
       ) : (
         <div className="relative">
           <Image
-            className="lg:w-full w-60 md:h-[400px] lg:h-[500px] flex-1 object-cover rounded-xl"
+            className="lg:w-full w-screen h-[1000px] md:h-[400px] lg:h-[500px] flex-1 object-cover md:rounded-xl"
             loader={() => src}
             src={src}
             width={100}
@@ -35,19 +35,21 @@ export default function Detail() {
             priority={true}
             alt="movie-picture-not-found"
           />
-          <div className="absolute top-0 lg:w-full w-60 md:h-[400px] lg:h-[500px] rounded-xl bg-black opacity-50"></div>
-          <div className="w-[285px] absolute top-[50px] left-32">
-            <CardMovie
-              urlImage={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
-              title={undefined}
-              year={undefined}
-              rating={data?.vote_average}
-              id={data?.id}
-              key={data?.id}
-              saved={undefined}
-            />
+          <div className="absolute top-0 lg:w-full md:w-60 md:h-[400px] lg:h-[500px] rounded-xl bg-black opacity-50"></div>
+          <div className="flex flex-row justify-center w-screen md:w-[285px] absolute top-[50px] md:left-32">
+            <div className="w-[250px] md:w-[285px]">
+              <CardMovie
+                urlImage={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
+                title={undefined}
+                year={undefined}
+                rating={data?.vote_average}
+                id={data?.id}
+                key={data?.id}
+                saved={undefined}
+              />
+            </div>
           </div>
-          <div className="absolute top-[50px] left-[460px] font-bold text-3xl text-white">
+          <div className="absolute bottom-[100px] md:top-[50px] left-10 right-10 md:right-0 md:left-[460px] font-bold text-3xl text-white">
             {data?.title ?? data?.name}{" "}
             <span className="font-normal text-slate-200">({year})</span>
             <div className="flex flex-row gap-1 mt-3">
@@ -72,19 +74,21 @@ export default function Detail() {
             <h3 className="text-xl font-normal opacity-80 italic mt-7 mb-2">
               {data?.tagline}
             </h3>
-            <h3 className="text-2xl font-semibold mt-3 mb-2">Overview</h3>
-            <p className="text-[16px] leading-5 font-normal pr-5">
+            <h3 className="text-xl sm:text-2xl font-semibold mt-3 mb-2">
+              Overview
+            </h3>
+            <p className="text-[13px] sm:text-[16px] leading-5 font-normal pr-5">
               {data?.overview ?? "Haven't given overview yet."}
             </p>
           </div>
-          <p className="absolute bottom-16 right-5  text-white">
+          <p className="absolute bottom-16 right-5 text-white">
             Production Companies:
           </p>
-          <div className="flex flex-row gap-1 absolute bottom-5 right-5">
+          <div className="flex flex-row gap-1 absolute bottom-5 right-3 md:right-5 px-3 md:px-0">
             {data?.production_companies.map((logo) => (
               <div className="flex justify-center bg-white p-1">
                 <Image
-                  className="w-[50px] h-[25px] rounded-sm"
+                  className="max-w-[50px] h-[25px] rounded-sm"
                   loader={() =>
                     `https://image.tmdb.org/t/p/w500${logo?.logo_path}`
                   }
